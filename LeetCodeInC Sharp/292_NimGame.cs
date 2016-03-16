bae0894677d47f460e7861fe710191861844b761
lyly513 <lyfcsc@hotmail.com>
2016-03-16 12:38:59 +0800

    first

---
+++ LeetCodeInC Sharp/292_NimGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInC_Sharp
+{
+    public class Solution
+    {
+        public bool CanWinNim(int n)
+        {
+            if (n <= 3)
+            {
+                return true;
+            }
+            else if ((n % 4 == 0))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
